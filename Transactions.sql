e87a385dd7510abb3a5a3ef23c95835e1f7afebe
Adem GUETTAF <93988396+Adem-developer@users.noreply.github.com>
2024-12-23 00:05:47 +0100

    Update Transactions

--- Transactions.sql
+++ Transactions.sql
@@ -1,15 +1,18 @@
 -- Transaction 1
-BEGIN;
 
-INSERT INTO Commandes (ID_Client, ID_Commande, Date_Commande, Statut, Total_Commande, ID_Employe)
-VALUES (9, 23, '20241216', 'EN_COURS', 5, 3)
-RETURNING ID_Commande;
+DO $$
+DECLARE
+    return_value INTEGER;
+BEGIN
 
-INSERT INTO Details_Commande(ID_Commande, ID_Plat, Quantite)
-VALUES (23, 4, 3); 
+    INSERT INTO Commandes (ID_Client, ID_Employe, Date_Commande, Statut)
+    VALUES (4, 2, '20241216', 'EN_COURS') 
+    RETURNING ID_Commande INTO return_value;
 
-COMMIT;
+    INSERT INTO Details_Commande(ID_Commande, ID_Plat, Quantite)
+    VALUES (return_value, 5, 3);
 
+END $$;
 
 -- Transaction 2
 BEGIN;
